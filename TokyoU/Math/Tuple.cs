feb918948b5c0e14d78ca2f658aa94c6cb25a26d
sakuyui <405544641@qq.com>
2020-06-28 13:41:07 +0800

    iniy

---
+++ TokyoU/Math/Tuple.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace TokyoU.Math
+{
+    public class Tuple<T1,T2>
+    {
+        public T1 Key;
+        public T2 Val;
+
+        public Tuple(T1 key, T2 val)
+        {
+            this.Key = key;
+            this.Val = val;
+        }
+
+        public Tuple()
+        {
+            Key = default(T1);
+            Val = default(T2);
+        }
+
+        public Object this[int index]
+        {
+            get
+            {
+                if (index == 0) return Key;
+                return Val;
+            }
+            set
+            {
+                if (index == 0) this.Key = (T1)value;
+                else
+                {
+                    this.Val = (T2) Val;
+                }
+            }
+        }
+
+        public Tuple<T3, T4> ConvertTo<T3, T4>()
+        {
+            Tuple<T3,T4> tuple = new Tuple<T3, T4>();
+            tuple.Key = (T3)((dynamic)this.Key);
+            tuple.Val = (T4) ((dynamic) this.Val);
+            return tuple;
+        }
+
+        public override string ToString()
+        {
+            return "(" + Key.ToString() + "," + Val.ToString() + ")";
+        }
+    }
+}
--- TokyoU/Program.cs
+++ TokyoU/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace TokyoU
+﻿using System;
+
+namespace TokyoU
 {
+    //Counter
+    //DataFrame
+    //Matrix
+    //Tuple
+    //Vector
+    //调度算法
     internal class Program
     {
         public static void Main(string[] args)
         {
+            //========================Tuple============================
+            Math.Tuple<int,double> tuple1 = new Math.Tuple<int, double>(10,10.1);
+            Console.WriteLine(tuple1);
+            tuple1.Key += 1;
+            Console.WriteLine(tuple1.ConvertTo<double,int>());
+            
         }
     }
 }
